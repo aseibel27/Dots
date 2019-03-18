265897f939a6c34882a03c47a1a9de7a33d43a42
aseibel27 <48417087+aseibel27@users.noreply.github.com>
2019-03-18 19:12:38 -0400

    Add files via upload
    Changed user key input to handle forward movement and turning instead of changing x and y location.  Added orientation property to class Ball so in the future orientation doesn't always match agent's velocity, but haven't implemented this yet.

--- src/Dots.java
+++ src/Dots.java
@@ -26,8 +26,6 @@ public class Dots extends PApplet{
     public void setup() {
         noStroke(); // default disabled outline
         fill(204, 204, 204); // default color
-        balls[0].velocity.x = 0;
-        balls[0].velocity.y = 0;
     }
     
     // draws background, redraws Balls
@@ -40,7 +38,7 @@ public class Dots extends PApplet{
         // for each ball
         for (Ball b : balls) {
             b.update(); // update position, velocity
-            b.display(b.color,b.viewRadius,b.viewAngle,b.viewColor); // redraw
+            b.display(); // redraw
             b.checkBoundaryCollision(); // update velocity upon boundary collision
         }
         
@@ -52,18 +50,18 @@ public class Dots extends PApplet{
         } 
     }
         
-    static boolean NORTH, SOUTH, WEST, EAST;
+    static boolean FORWARD, BACKWARD, TURNRIGHT, TURNLEFT;
 
     public void keyPressed() {
       
       redraw();   //  queue draw()
-      print("Fuck");
+      
       final int k = keyCode;
           
-      if (k == UP    | k == 'W')   NORTH = true;
-      else if (k == DOWN  | k == 'S')   SOUTH = true;
-      else if (k == LEFT  | k == 'A')   WEST  = true;
-      else if (k == RIGHT | k == 'D')   EAST  = true;
+      if      (k == UP    | k == 'W')   FORWARD = true;
+      else if (k == DOWN  | k == 'S')   BACKWARD = true;
+      else if (k == LEFT  | k == 'A')   TURNLEFT  = true;
+      else if (k == RIGHT | k == 'D')   TURNRIGHT  = true;
     }
     
     public void keyReleased() {
@@ -71,27 +69,29 @@ public class Dots extends PApplet{
     
       final int k = keyCode;
     
-      if      (k == UP    | k == 'W')   NORTH = false;
-      else if (k == DOWN  | k == 'S')   SOUTH = false;
-      else if (k == LEFT  | k == 'A')   WEST  = false;
-      else if (k == RIGHT | k == 'D')   EAST  = false;
+      if      (k == UP    | k == 'W')   FORWARD = false;
+      else if (k == DOWN  | k == 'S')   BACKWARD = false;
+      else if (k == LEFT  | k == 'A')   TURNLEFT  = false;
+      else if (k == RIGHT | k == 'D')   TURNRIGHT  = false;
     }
-
-    static void moveObj(PVector aVel) {
-      if (NORTH & aVel.y > -5) aVel.y -= 1;
-      else if (!NORTH & aVel.y < 0) aVel.y *= 0.9;
-      if (SOUTH & aVel.y < 5)  aVel.y += 1;
-      else if (!SOUTH & aVel.y > 0) aVel.y *= 0.9;
-      if (WEST & aVel.x > -5)  aVel.x -= 1;
-      else if (!WEST & aVel.x < 0) aVel.x *= 0.9;
-      if (EAST & aVel.x < 5)   aVel.x += 1;
-      else if (!EAST & aVel.x > 0) aVel.x *= 0.9;
-    }
+// need to add both orientation and velocity vectors
+    static float speed, minSpeed = 0.5f, maxSpeed = 10f, rotSpeed = 0.07f, friction = 0.9f, accel = 1.2f;
+    
+    void moveObj(PVector aVel) {
+      speed = aVel.mag();
+      if (FORWARD & speed < minSpeed ) aVel.normalize().mult(minSpeed);
+      else if (FORWARD & speed >= minSpeed & aVel.mag() < maxSpeed) aVel.mult(accel);
+      else if (FORWARD & speed >= maxSpeed) aVel.normalize().mult(maxSpeed);
+      else if (!FORWARD & speed > 0) aVel.mult(friction);
+      if (TURNLEFT)  aVel.rotate(-rotSpeed);
+      if (TURNRIGHT) aVel.rotate(rotSpeed);
+      if (FORWARD | TURNRIGHT | TURNLEFT) balls[0].orientation=balls[0].velocity;
+     }
 }
 
 class Ball {
     PApplet parent;
-    PVector position, velocity;
+    PVector position, velocity, orientation;
     int color, viewColor;
     float radius, m, viewRadius, viewAngle;
     final double PI = 3.1415926536;
@@ -101,6 +101,7 @@ class Ball {
       position = new PVector(x, y); // set position to specified x and y
       velocity = PVector.random2D(); // unit vector random direction
       velocity.mult(3); // set velocity magnitude
+      orientation = PVector.random2D();
       radius = r_; // set specified radius
       m = radius*.1f; // momentum?
       color = parent.color(c[0],c[1],c[2]);
@@ -227,10 +228,10 @@ class Ball {
       }
     }
     
-    public void display(int color, float vr, float va, int vColor) { 
+    public void display() { 
         parent.fill(color);
         parent.ellipse(position.x, position.y, radius*2, radius*2);
-        parent.fill(vColor);
-        parent.arc(position.x,position.y,vr*2,vr*2,velocity.heading()-va/2,velocity.heading()+va/2);
+        parent.fill(viewColor);
+        parent.arc(position.x,position.y,viewRadius*2,viewRadius*2,velocity.heading()-viewAngle/2,velocity.heading()+viewAngle/2);
     }
   }
